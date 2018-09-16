33a3614a450af82eab5460569c3883f6a6e95c73
pav-Thinkpad-W520 <alexandrupavilcu@gmail.com>
2018-09-17 01:01:57 +0300

    updated listsContinued

--- IdeaProjects/Lists/listsContinued.py
+++ IdeaProjects/Lists/listsContinued.py
@@ -9,13 +9,21 @@
 #
 # print(list("The lists are equal"))
 
-even = [2, 4, 6, 8]
+# test change for git
+
+# even = [2, 4, 6, 8]
 
 # another_even = even
 # another_even = list(even)
-another_even = sorted(even, reverse=True)
-print(another_even is even)
+# another_even = sorted(even, reverse=True)
+# print(another_even is even)
+#
+# another_even.sort(reverse=True)
+# print(even)
+# print(another_even)
 
-another_even.sort(reverse=True)
-print(even)
-print(another_even)
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = [even, odd]
+print(numbers)
