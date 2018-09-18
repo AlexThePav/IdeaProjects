cad50f4c8ea9df7d5d900434149e84d72710db0d
pav-Thinkpad-W520 <alexandrupavilcu@gmail.com>
2018-09-18 23:00:28 +0300

    Added Tuples

---
+++ IdeaProjects/Tuples/tuples.py
@@ -0,0 +1,46 @@
+# t = "a", "b", "c"
+# print(t)
+#
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Night Flight", "Budgie", 1981
+imelda = "More Mayhem", "Imelda May", 2011, \
+         (1, "Pulling the Rug"),\
+         (2, "Psycho"),\
+         (3, "Mayhem"),\
+         (4, "Kentish Town Waltz")
+
+# metallica = "Ride the Lightning", "Metallica", 1984
+#
+# # print(metallica)
+# # print(metallica[0])
+# # print(metallica[1])
+# # print(metallica[2])
+# #
+# # imelda = imelda[0], "Imelda May", imelda[2]
+# # print(imelda)
+#
+# metallica2 = ["Ride the Lightning", "Metallica", 1984]
+# print(metallica2)
+#
+# metallica2.append("Rock")
+# title, artist, year, genre = metallica2
+# #
+# # print(title)
+# # print(artist)
+# # print(year)
+#
+# imelda.append("Jazz")
+
+print(imelda)
+title, artist, year, track1, track2, track3, track4 = imelda
+print(title)
+print(artist)
+print(year)
+print(track1)
+print(track2)
+print(track3)
+print(track4)
