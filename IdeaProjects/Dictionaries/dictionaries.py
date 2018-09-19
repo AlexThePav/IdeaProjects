9b8c895c193aaca7ac04d3f6c8ddb3659a7bd438
pav-Thinkpad-W520 <alexandrupavilcu@gmail.com>
2018-09-20 00:28:04 +0300

    Dictionaries

--- IdeaProjects/Dictionaries/dictionaries.py
+++ IdeaProjects/Dictionaries/dictionaries.py
@@ -17,12 +17,48 @@ fruit = {"orange": "a sweet orange citrus fruit",
 # print(fruit)
 print(fruit)
 
-while True:
-    dict_key = input("Please enter a fruit: ")
-    if dict_key == "quit":
-        break
-    if dict_key in fruit:
-        description = fruit.get(dict_key)
-        print(description)
-    else:
-        print("We don't have a " + dict_key)
+# while True:
+#     dict_key = input("Please enter a fruit: ")
+#     if dict_key == "quit":
+#         break
+#     # description = fruit.get(dict_key, "We don't have a " + dict_key)
+#     # print(description)
+#     if dict_key in fruit:
+#         description = fruit.get(dict_key)
+#         print(description)
+#     else:
+#         print("We don't have a " + dict_key)
+
+# for snack in fruit:
+#     print(fruit[snack])
+
+# for i in range(10):
+#     for snack in fruit:
+#         print(snack + " is " + fruit[snack])
+#     print("-" * 40)
+
+# ordered_keys = list(fruit.keys())
+# ordered_keys.sort()
+# ordered_keys = sorted(list(fruit.keys()))
+# for f in ordered_keys:
+#     print(f + " - " + fruit[f])
+
+# for val in fruit.values():
+#     print(val)
+#
+# fruit_keys = fruit.keys()
+# print(fruit_keys)
+#
+# fruit["tomato"] = "not nice with ice cream"
+# print(fruit_keys)
+
+
+print(fruit.items())
+f_tuple = tuple(fruit.items())
+print(f_tuple)
+
+for snack in f_tuple:
+    item, description = snack
+    print(item + " is " + description)
+
+print(dict(f_tuple))
