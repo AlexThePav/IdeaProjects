c277391d66a6288eeb373255b701f8914d24feee
pav-thinkpad-w520 <alexandrupavilcu@gmail.com>
2018-09-23 19:01:32 +0300

    Sets

---
+++ IdeaProjects/Sets/challenge.py
@@ -0,0 +1,22 @@
+# Create a program that takes some text and returns a list of
+# all the characters in the text that are not vowels, sorted in
+# alphabetical order.
+#
+# You can either enter from the keyboard or initialize
+# a string variable with the string
+
+text = input("Please enter some text: ")
+
+# vowels = "aeiouAEIOU"
+# nonVowels = set()
+#
+# for char in text:
+#     if char not in vowels:
+#         nonVowels.add(char)
+#
+# sortedNonVowels = sorted(nonVowels)
+# print(sortedNonVowels)
+
+vowels = frozenset("aeiouAEIOU")
+finalSet = set(text).difference(vowels)
+print(", ".join(finalSet))
