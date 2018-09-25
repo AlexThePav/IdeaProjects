2fd4ddf0fc79b31bba6411be95b26119a0ad6bd7
pav-thinkpad-w520 <alexandrupavilcu@gmail.com>
2018-09-25 22:55:46 +0300

    Shelve

--- IdeaProjects/Shelve/shelve.py
+++
@@ -1,8 +0,0 @@
-import shelve
-
-with shelve.open('ShelfTest') as fruit:
-    fruit['orange'] = 'a sweet, orange, citrus fruit'
-    fruit['apple'] = 'good for making cider'
-    fruit['lemon'] = 'a sweet, orange, citrus fruit'
-    fruit['grape'] = 'a sweet, orange, citrus fruit'
-    fruit['lime'] = 'a sweet, orange, citrus fruit'
