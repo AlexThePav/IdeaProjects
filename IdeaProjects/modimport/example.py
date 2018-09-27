9bf80ffe7930b52084ebf40d47e783f94f2875da
pav-amdeus <alexandrupavilcu@gmail.com>
2018-09-28 00:00:53 +0300

    modimport

---
+++ IdeaProjects/modimport/example.py
@@ -0,0 +1,20 @@
+import shelve
+import random
+
+# help(shelve)
+help(random.randint)
+
+# for obj in dir(shelve.Shelf):
+#     if obj[0] != '_':
+#         print(obj)
+#
+
+
+# print(dir())
+# print()
+# print(dir(shelve))
+
+# print(dir())
+# # print(dir(__builtins__))
+# for m in dir(__builtins__):
+#     print(m)
---
+++ IdeaProjects/modimport/turtledemo.py
@@ -0,0 +1,12 @@
+# done = "Well done, you have finished your drawing"
+from turtle import *
+# from turtle import forward, right, done
+# import turtle
+
+forward(150)
+right(250)
+circle(75)
+forward(150)
+
+done()
+# print(done)
