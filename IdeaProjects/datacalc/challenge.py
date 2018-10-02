06fb620967a55b6f887fd31a21cda7bffcf32cac
pav-amdeus <alexandrupavilcu@gmail.com>
2018-10-02 23:03:27 +0300

    timezones

--- IdeaProjects/datacalc/challenge.py
+++ IdeaProjects/datacalc/challenge.py
@@ -7,7 +7,7 @@
 
 import time
 
-print("time:\t\t\t", time.get_clock_info("time"))
-print("monotonic:\t\t", time.get_clock_info("monotonic"))
+print("time:\t\t", time.get_clock_info("time"))
+print("monotonic:\t", time.get_clock_info("monotonic"))
 print("perf_counter:\t", time.get_clock_info("perf_counter"))
 print("process_time:\t", time.get_clock_info("process_time"))
