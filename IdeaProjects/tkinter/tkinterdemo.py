68b2d33a0965c884f601befee0108b4c70da7a4d
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2018-10-03 23:40:04 +0300

    tkinter stuff

--- IdeaProjects/tkinter/tkinterdemo.py
+++ IdeaProjects/tkinter/tkinterdemo.py
@@ -1,7 +1,14 @@
-#test c
-import tkinter
+try:
+    import tkinter
+except ImportError: # python2
+    import Tkinter as tkinter
 
 print(tkinter.TkVersion)
 print(tkinter.TclVersion)
 
-tkinter._test()
+# tkinter._test()
+mainWindow = tkinter.Tk()
+
+mainWindow.title("Hello WOrld")
+mainWindow.geometry('640x480+2885+400')
+mainWindow.mainloop()
