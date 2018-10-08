7c9a7a163a043745815902290bd0888abb7751b7
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2018-10-09 00:15:10 +0300

    chessboard

--- IdeaProjects/functions/functions.py
+++ IdeaProjects/functions/functions.py
@@ -1,5 +1,28 @@
 def python_food():
-    print("spam and eggs")
+    width = 80
+    text = "Spam and eggs"
+    left_margin = (width - len(text)) // 2
+    print(" " * left_margin, text)
 
+
+def centre_text(*args, sep=' '):
+    text = ""
+    for arg in args:
+        text += str(arg) + sep
+    text = text.strip(sep)
+    left_margin = (80 - len(text)) // 2
+    return " " * left_margin + text
+
+# with open("centred", mode='w') as centred_file:
 # call the function
-python_food()
+
+
+with open("menu", "w") as menu:
+    s1 = centre_text("spam and eggs")
+    print(s1, file=menu)
+    s2 = centre_text("spam, spam and eggs")
+    print(s2, file=menu)
+    s3 = centre_text(12)
+    print(s3, file=menu)
+    print(centre_text("spam, spam, spam and spam"), file=menu)
+    print(centre_text("first", "second", 3, 4, "spam", sep=":"), file=menu)
