df9825cdb4affb771b5e71fe6936d8e3aaeaf2a2
Alexandru Pavilcu <alexandrupavilcu@gmail.com>
2018-11-15 00:23:52 +0200

    vmapyreking

--- IdeaProjects/Game/main.py
+++ IdeaProjects/Game/main.py
@@ -1,26 +1,34 @@
-from enemy import Troll, Vampyre
+from enemy import VampyreKing
 
-ugly_troll = Troll("Pug")
-print("Ugly troll - {}".format(ugly_troll))
+# ugly_troll = Troll("Pug")
+# print("Ugly troll - {}".format(ugly_troll))
+#
+# another_troll = Troll("Ug")
+# print("Another troll - {}".format(another_troll))
+#
+# brother = Troll("Urg")
+# print(brother)
+#
+# vlad = Vampyre("Vlad")
+# print(vlad)
+#
+# ugly_troll.grunt()
+# another_troll.grunt()
+# brother.grunt()
+# brother.take_damage(2)
+#
+# print("=" * 40)
+# another_troll.take_damage(30)
+# print(another_troll)
+#
+# # while vlad.alive:
+# #     vlad.take_damage(1)
+# #     print(vlad)
+#
+# vlad._lives = 0
+# vlad._hit_points = 1
+# print(vlad)
 
-another_troll = Troll("Ug")
-print("Another troll - {}".format(another_troll))
-
-brother = Troll("Urg")
-print(brother)
-
-vlad = Vampyre("Vlad")
-print(vlad)
-
-ugly_troll.grunt()
-another_troll.grunt()
-brother.grunt()
-brother.take_damage(2)
-
-print("=" * 40)
-another_troll.take_damage(30)
-print(another_troll)
-
-while vlad.alive:
-    vlad.take_damage(1)
-    print(vlad)
+dunitru = VampyreKing("Dunitru")
+while dunitru._alive:
+    dunitru.take_damage(8)
